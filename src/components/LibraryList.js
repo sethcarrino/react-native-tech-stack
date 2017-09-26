4cac49502c4458b2ea7b36cd64e5c9eaeb0d6c89
sethcarrino <sethcarrino@gmail.com>
2017-09-26 10:57:11 -0400

    Applied styling to components using styled components

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 import ListItem from './ListItem';
